4c541172dc1851c858a5f17fa6f2324e0fee6f31
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2026-04-06 14:04:11 +0200

    M306/03/01/*

---
+++ 03/03_Solutions/01/00-Exercise.md
@@ -0,0 +1,17 @@
+# Projekt-Planung
+
+[<img src="img/01.png" width="400"/>](PM)
+
+## **Aufgabe 1: Wahl des Vorgehensmodells**
+
+Sie erhalten 75 Elemente (Tasks + Ressourcen) ungeordnet (flache Liste, keine Hierarchie, keine Abhängigkeiten, keine Ressourcen). Die Studierenden müssen:
+
+Tasks den richtigen Phasen zuordnen und verschachteln
+Abhängigkeiten (Finish-to-Start) definieren
+Ressourcen zuweisen
+Timeline ableiten
+
+Musterlösung.gantt
+Vollständig geplantes Projekt 06.04.–26.08.2026 mit:
+PhaseZeitraumTasksAnforderungen06.04. – 01.05.13 (inkl. Meilenstein)Design04.05. – 22.05.12Realisation25.05. – 06.07.22Testing07.07. – 07.08.13Abnahme10.08. – 26.08.10
+11 Personen mit bewusst reduzierter Auslastung (sie arbeiten parallel in anderen Projekten): Peter Müller (PM), Anna Bauer (60%), Thomas Keller (50%), Sandra Huber (70%), Michael Schmid (60%), Felix Wagner (80%), Laura Fischer (70%), Jonas Weber (60%), Lena Hoffmann (50%), Klaus Richter (70%), Maria Schneider (60%).
---
+++ 03/03_Solutions/prep.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+LIB_LOC=$(hostname)
+if [[ "$LIB_LOC" -eq "eigerw" ]]
+then
+  LIB='/c/Data/Development/wp_appi-repo/00_Resources/bin/prep-lib.sh'
+fi
+if [ ! -f $LIB ]; then
+    echo $LIB does not exist!
+    exit 2
+  fi
+source $LIB
+
+#----------------------------------------
+if test $# -lt 1; then
+  printf "%s\n" \
+    "Error: Provide at least 1 argument:" \
+    "Prep number n" \
+    "Exit script."
+  exit 1
+fi
+
+
+function initPart() {
+  local part=$1
+  createTargetDir "${DEST}/${part}"
+}
+
+function createPart() {
+  local part=$1
+  local file="${part}/$2"
+  local lines=$3
+  initPart ${part}
+  rml.sh  ${file} "${lines}" > "${DEST}/${file}"
+  copyImages ${file}
+
+}
+
+function copyImages() {
+  local -a file=$1
+  dirName="$(dirname "${file}")"
+  if [[ -d "$dirName/img" ]]
+  then
+    echo "Copy complete $dirName/img .."
+    cp -r "$dirName/img" "${DEST}/$dirName"
+  fi
+}
+
+
+DEST="../02_Exercises"
+
+case $1 in
+1)
+  createPart 01 "00-Exercise.md" "18,28;44,58;79,90;110,118;133,143"
+  ;;
+2)
+  createPart 02 "00-Exercise.md" "9,18;25,35;47,51;59,71;79,86;94,115"
+  ;;
+*)
+  echo "Incorrect choice entered!"
+  ;;
+esac
